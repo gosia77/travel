3486c4f56819ff968de3d1fa1e11aa43a6cd14c9
unknown <malgorzataserafinlatka@gmail.com>
2023-12-06 15:53:25 +0100

    first part of new website

---
+++ hamburger.js
@@ -0,0 +1,19 @@
+const hamburger = document.querySelector('.hamburger');
+const menu = document.querySelector('.top-menu');
+const line1 = document.querySelector('.line1');
+const line2 = document.querySelector('.line2');
+const line3 = document.querySelector('.line3');
+
+
+const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
+
+if (vw < 1024) {
+    hamburger.addEventListener('click', () => {
+        menu.classList.toggle('top-menu-active');
+
+        line1.classList.toggle('line1-active');
+        line2.classList.toggle('line2-active');
+        line3.classList.toggle('line3-active');
+
+        });
+    }
